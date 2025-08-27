b8bf043ec77ce364df0d41e6fc223971181985e0
VamsiYalamati <code4vamsi@gmail.com>
2025-08-27 06:50:21 +0000

    container config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,19 @@
+const accountId=12343747
+//in js use of ";" is has no issue use or not 
+console.log(accountId);
+let accountEmail="vamksdoi@eauosc.com"
+var accountPassword="121312"
+/*use of var is stictly avoided because  it doesnot follow the bleck and functional scope of the variable
+so in the modern days the developers use only const and let*/
+accountCity="hyderabad"//we can declare a variable without any declaration in the js which is not the 
+//correct practice
+// accountId=3264816 not allowed
+accountEmail="sjdsdakfbkusb"
+accountPassword="123586125"
+accountCity="bhopal"
+
+
+let accountState;
+//the output is undefineed not garbage
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
