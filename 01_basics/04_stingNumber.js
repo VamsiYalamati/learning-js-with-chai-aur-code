fe5f4f78a6fc86710049825b9eba16c1105b5a33
VamsiYalamati <code4vamsi@gmail.com>
2025-08-27 09:21:57 +0000

    string number

---
+++ 01_basics/04_stingNumber.js
@@ -0,0 +1,23 @@
+let value=33
+let negValue=-value
+console.log(negValue)//-33
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)//which is nothing but the 2^3
+console.log(2/2)
+console.log(2%2)
+
+
+console.log("1"+5)//first string output is also string
+console.log(1+3+"5")//first number output is also number addition
+
+console.log(true)//true
+console.log(+true);//1
+//console.log(true+);//error
+
+let num1,num2,num3;
+num1=num2=num3=2;
+console.log(num1);//2
+
