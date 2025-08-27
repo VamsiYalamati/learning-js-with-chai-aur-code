e9602d7a3252e067b203de83c64c92aaa76cb964
VamsiYalamati <code4vamsi@gmail.com>
2025-08-27 12:10:29 +0000

    stack and heap memory

--- 01_basics/06_dataSummary.js
+++ 01_basics/06_dataSummary.js
@@ -18,5 +18,63 @@ let myObj={
 let myFunction=function(){
     console.log("hello world")
 }
-
 console.log(typeof bigNumber)
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// memory 2 twoTypes
+// 1.heap(non-primitive)
+// 2.stack9(primitive)
+
+let myName="vamsi"
+let friendName="dhanush"
+
+console.log(myName);
+console.log(friendName);
+//copy generated
+
+
+ let userOne={
+    userName:"vamsi",//use of colon not =
+    age:12//use of colon not =
+ }
+ let userTwo=userOne
+ userTwo.userName="dhanush";
+ console.log(userOne.userName);
+console.log(userTwo.userName);
+
+//pointed to the same memory
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
